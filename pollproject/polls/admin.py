a4c94edba960442d1252a22182a1c586da690b8e
oopanpan <oopanpan@gmail.com>
2021-08-12 20:31:43 -0700

    admin page configured in admin.py

--- pollproject/polls/admin.py
+++ pollproject/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+#Admin page site title and text display
+admin.site.site_header = 'Poll Project Admin'
+admin.site.site_title = 'Poll Project Admin Area'
+admin.site.index_title = 'Welcome to the Admin Area'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),]
+    inlines = [ChoiceInline]
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
--- pollproject/polls/models.py
+++ pollproject/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
-    pub_data = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published')
     
     def __str__(self):
         return self.question_text
